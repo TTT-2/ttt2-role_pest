6488d3013190ac8861f6df5f5a4244e0c47186a5
Pythagorion <43889730+Pythagorion@users.noreply.github.com>
2020-06-29 18:52:59 +0200

    Add updated langfiles and filled them (#1)
    Because the file structure differs from the previous one, I will commit this as a PR.

---
+++ lua/lang/english/plag.lua
@@ -0,0 +1,16 @@
+L = LANG.GetLanguageTableReference("english")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[PLAG.name] = "Plague-Host"
+L[TEAM_PLAGUEHOST] = "TEAM Pestilence"
+L["hilite_win_" .. TEAM_PLAGUEHOST] = "PESTILENCE WON!"
+L["win_" .. TEAM_PLAGUEHOST] = "The Pest wins!"
+L["info_popup_" .. PLAG.name] = [[You are the Plague-Host!
+Infect all the others silently, to transform into Pestilence and become immortal!]]
+L["body_found_" .. PLAG.abbr] = "They were a Plague-Host."
+L["search_role_" .. PLAG.abbr] = "This person was a Plague-Host!"
+L["ev_win_" .. TEAM_PLAGUEHOST] = "The evil Pest wins!"
+L["target_" .. PLAG.name] = "Plague-Host"
+L["ttt2_desc_" .. PLAG.name] = [[The Plague-Host transform into Pestilence and wins alone then!]]
+
+--OTHER ROLE LANGUAGE STRINGS
