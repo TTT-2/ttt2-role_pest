6fae11431772798517be9bebcf6edf4f735f5577
Tim Goll <tim@timgoll.de>
2020-07-04 00:28:26 +0200

    fixed phiole throw animation and added glow effect + sound handling

--- gamemodes/terrortown/entities/entities/ttt_phiole/shared.lua
+++ gamemodes/terrortown/entities/entities/ttt_phiole/shared.lua
@@ -8,16 +8,58 @@ ENT.model = Model("models/props_junk/PopCan01a.mdl")
 ENT.CanHavePrints = true
 ENT.CanUseKey = true
 
-function ENT:Initialize()
-	self:SetModel(self.model)
+if SERVER then
+	local soundsPlace = {
+		"phiole.place_1",
+		"phiole.place_2",
+		"phiole.place_3",
+		"phiole.place_4",
+		"phiole.place_5",
+	}
 
-	self:PhysicsInit(SOLID_VPHYSICS)
-	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_VPHYSICS)
+	function ENT:Initialize()
+		self:SetModel(self.model)
 
-	local phys = self:GetPhysicsObject()
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
 
-	if IsValid(phys) then
-		phys:Wake()
+		local phys = self:GetPhysicsObject()
+
+		if IsValid(phys) then
+			phys:Wake()
+		end
+
+		self:EmitSound(table.Random(soundsPlace))
+	end
+end
+
+if CLIENT then
+	function ENT:Initialize()
+		self:SetGlowColor(Color(105, 50, 65, 255))
+	end
+
+	function ENT:SetGlowColor(color)
+		self.color = color
+	end
+
+	function ENT:LightUp(color)
+		if not IsColor(color) then return end
+
+		local dlight = DynamicLight(self:EntIndex())
+
+		dlight.r = color.r
+		dlight.g = color.g
+		dlight.b = color.b
+
+		dlight.brightness = 5
+		dlight.Decay = 1000
+		dlight.Size = 25
+		dlight.DieTime = CurTime() + 0.1
+		dlight.Pos = self:GetPos()
+	end
+
+	function ENT:Think()
+		self:LightUp(self.color)
 	end
 end
---
+++ lua/autorun/sh_pestilence_setup.lua
@@ -0,0 +1,71 @@
+sound.Add({
+	name = "phiole.place_1",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_place_1.wav"
+})
+
+sound.Add({
+	name = "phiole.place_2",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_place_2.wav"
+})
+
+sound.Add({
+	name = "phiole.place_3",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_place_3.wav"
+})
+
+sound.Add({
+	name = "phiole.place_4",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_place_4.wav"
+})
+
+sound.Add({
+	name = "phiole.place_5",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_place_5.wav"
+})
+
+sound.Add({
+	name = "phiole.shatter_1",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_shatter_1.wav"
+})
+
+sound.Add({
+	name = "phiole.shatter_2",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_shatter_2.wav"
+})
+
+sound.Add({
+	name = "phiole.shatter_3",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/glass_shatter_3.wav"
+})
+
+sound.Add({
+	name = "phiole.steam",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 100,
+	sound = "phiole/steam.wav"
+})
