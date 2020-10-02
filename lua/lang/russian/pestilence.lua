e9be10abad52c431650ba4f72a109000bd2414fd
Blueberryy <igory.ygr200@gmail.com>
2020-10-02 21:41:26 +0500

    Create pestilence.lua

---
+++ lua/lang/russian/pestilence.lua
@@ -0,0 +1,25 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[PESTILENCE.name] = "Чума"
+L[TEAM_PESTILENCE] = "Команда чумного хозяина"
+L["hilite_win_" .. TEAM_PESTILENCE] = "ПОБЕДА ЧУМЫ"
+L["win_" .. TEAM_PESTILENCE] = "Чума победила!"
+L["info_popup_" .. PESTILENCE.name] = [[Вы чума!
+После того, как ваш хост заразил всех недостойных, вы теперь можете их всех устранить!]]
+L["body_found_" .. PESTILENCE.abbr] = "Он был чумой."
+L["search_role_" .. PESTILENCE.abbr] = "Этот человек был чумой!"
+L["ev_win_" .. TEAM_PESTILENCE] = "Злая чума победила!"
+L["target_" .. PESTILENCE.name] = "Чума"
+L["ttt2_desc_" .. PESTILENCE.name] = [[Чума победила в одиночку!]]
+
+L[PLAGUEHOST.name] = "Чумной хозяин"
+L["info_popup_" .. PLAGUEHOST.name] = [[Вы чумной хозяин!
+Тихо заразите всех остальных, чтобы превратиться в чуму и стать бессмертным!]]
+L["body_found_" .. PLAGUEHOST.abbr] = "Он был чумным хозяином."
+L["search_role_" .. PLAGUEHOST.abbr] = "Этот человек был чумным хозяином!"
+L["target_" .. PLAGUEHOST.name] = "Чумной хозяин"
+L["ttt2_desc_" .. PLAGUEHOST.name] = [[Чумной хозяин превратился в чуму и победил в одиночку!]]
+
+--OTHER ROLE LANGUAGE STRINGS
+
