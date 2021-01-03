84a27fab026904e5d21d647aec7677fadc53927e
Aram <dragolino6@mail.ru>
2021-01-03 23:06:43 +0300

    Agh... true minor change...

--- lua/lang/english/pestilence.lua
+++ lua/lang/english/pestilence.lua
@@ -6,7 +6,7 @@ L[TEAM_PESTILENCE] = "TEAM Pestilence"
 L["hilite_win_" .. TEAM_PESTILENCE] = "THE PESTILENCE WON"
 L["win_" .. TEAM_PESTILENCE] = "The Pest wins!"
 L["info_popup_" .. PESTILENCE.name] = [[You are now a Pestilence!
-Since every unworthy person have infected, you are now able to eliminate them all!]]
+Since every unworthy person got infected, you are now able to eliminate them all!]]
 L["body_found_" .. PESTILENCE.abbr] = "They were a Pestilence."
 L["search_role_" .. PESTILENCE.abbr] = "This person was a Pestilence!"
 L["ev_win_" .. TEAM_PESTILENCE] = "The evil Pest wins!"
