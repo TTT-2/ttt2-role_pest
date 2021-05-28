b0f234c7136250e5e775387ae66d7e4c8af8804a
Aram <dragolino6@mail.ru>
2021-05-28 13:09:35 +0300

    Localization changes and improvements (#4)

--- lua/lang/english/pestilence.lua
+++ lua/lang/english/pestilence.lua
@@ -2,24 +2,23 @@ L = LANG.GetLanguageTableReference("english")
 
 -- GENERAL ROLE LANGUAGE STRINGS
 L[PESTILENCE.name] = "Pestilence"
-L[TEAM_PESTILENCE] = "TEAM Plaguehost"
+L[TEAM_PESTILENCE] = "TEAM Pestilence"
 L["hilite_win_" .. TEAM_PESTILENCE] = "THE PESTILENCE WON"
 L["win_" .. TEAM_PESTILENCE] = "The Pest wins!"
-L["info_popup_" .. PESTILENCE.name] = [[You are the Pestilence!
-After your host infected all unworthy, you are now able to eliminate them all!]]
-L["body_found_" .. PESTILENCE.abbr] = "They were Pestilence."
-L["search_role_" .. PESTILENCE.abbr] = "This person was Pestilence!"
+L["info_popup_" .. PESTILENCE.name] = [[You are now a Pestilence!
+Since every unworthy person got infected, you are now able to eliminate them all!]]
+L["body_found_" .. PESTILENCE.abbr] = "They were a Pestilence."
+L["search_role_" .. PESTILENCE.abbr] = "This person was a Pestilence!"
 L["ev_win_" .. TEAM_PESTILENCE] = "The evil Pest wins!"
 L["target_" .. PESTILENCE.name] = "Pestilence"
 L["ttt2_desc_" .. PESTILENCE.name] = [[Pestilence wins alone!]]
 
 L[PLAGUEHOST.name] = "Plague-Host"
-L["info_popup_" .. PLAGUEHOST.name] = [[You are the Plague-Host!
+L["info_popup_" .. PLAGUEHOST.name] = [[You are a Plague-Host!
 Infect all the others silently, to transform into Pestilence and become immortal!]]
 L["body_found_" .. PLAGUEHOST.abbr] = "They were a Plague-Host."
 L["search_role_" .. PLAGUEHOST.abbr] = "This person was a Plague-Host!"
 L["target_" .. PLAGUEHOST.name] = "Plague-Host"
-L["ttt2_desc_" .. PLAGUEHOST.name] = [[The Plague-Host transform into Pestilence and wins alone then!]]
+L["ttt2_desc_" .. PLAGUEHOST.name] = [[Pestilence has infected and eliminated everyone!]]
 
 --OTHER ROLE LANGUAGE STRINGS
-
--- lua/lang/russian/pestilence.lua
+++ lua/lang/russian/pestilence.lua
@@ -2,24 +2,23 @@ L = LANG.GetLanguageTableReference("Русский")
 
 -- GENERAL ROLE LANGUAGE STRINGS
 L[PESTILENCE.name] = "Чума"
-L[TEAM_PESTILENCE] = "Команда чумного хозяина"
-L["hilite_win_" .. TEAM_PESTILENCE] = "ПОБЕДА ЧУМЫ"
+L[TEAM_PESTILENCE] = "Команда чумы"
+L["hilite_win_" .. TEAM_PESTILENCE] = "Победа чумы"
 L["win_" .. TEAM_PESTILENCE] = "Чума победила!"
-L["info_popup_" .. PESTILENCE.name] = [[Вы чума!
-После того, как ваш хост заразил всех недостойных, вы теперь можете их всех устранить!]]
+L["info_popup_" .. PESTILENCE.name] = [[Вы стали чумой!
+Поскольку каждый недостойный человек заразился, теперь вы можете их всех уничтожить!]]
 L["body_found_" .. PESTILENCE.abbr] = "Он был чумой."
 L["search_role_" .. PESTILENCE.abbr] = "Этот человек был чумой!"
 L["ev_win_" .. TEAM_PESTILENCE] = "Злая чума победила!"
 L["target_" .. PESTILENCE.name] = "Чума"
 L["ttt2_desc_" .. PESTILENCE.name] = [[Чума победила в одиночку!]]
 
-L[PLAGUEHOST.name] = "Чумной хозяин"
-L["info_popup_" .. PLAGUEHOST.name] = [[Вы чумной хозяин!
-Тихо заразите всех остальных, чтобы превратиться в чуму и стать бессмертным!]]
-L["body_found_" .. PLAGUEHOST.abbr] = "Он был чумным хозяином."
-L["search_role_" .. PLAGUEHOST.abbr] = "Этот человек был чумным хозяином!"
-L["target_" .. PLAGUEHOST.name] = "Чумной хозяин"
-L["ttt2_desc_" .. PLAGUEHOST.name] = [[Чумной хозяин превратился в чуму и победил в одиночку!]]
+L[PLAGUEHOST.name] = "Носитель чумы"
+L["info_popup_" .. PLAGUEHOST.name] = [[Вы носитель чумы!
+Незаметно заразите всех остальных, чтобы превратиться в чуму и стать неуязвимым!]]
+L["body_found_" .. PLAGUEHOST.abbr] = "Он был носителем чумы."
+L["search_role_" .. PLAGUEHOST.abbr] = "Этот человек был носителем чумы!"
+L["target_" .. PLAGUEHOST.name] = "Носитель чумы"
+L["ttt2_desc_" .. PLAGUEHOST.name] = [[Чума всех заразила и уничтожила!]]
 
 --OTHER ROLE LANGUAGE STRINGS
-
